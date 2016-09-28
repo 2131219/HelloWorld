603be3c15dee41197c080aa6ec08219c40080a13
Carlos Santos <2131219@my.ipleiria.pt>
2016-09-28 22:25:41 +0100

    botao close

--- app/src/main/java/com/example/carlo/helloworld/NextActivity.java
+++ app/src/main/java/com/example/carlo/helloworld/NextActivity.java
@@ -1,7 +1,10 @@
 package com.example.carlo.helloworld;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class NextActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class NextActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
+
+        Button nextButton = (Button)findViewById(R.id.button2);
+        nextButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                NextActivity.this.finish();
+            }
+        });
     }
 }
